4b2dc811fd871047b1920b6749f31d7c1e00612f
Nyxaris <85733843+Nyxaris@users.noreply.github.com>
2023-03-10 17:27:10 +0300

    Update Nyx_Code.py
    Добавлены ключевые слова и улучшена обработка случайного ответа на вопрос

--- Nyx_Code.py
+++ Nyx_Code.py
@@ -5,7 +5,13 @@ import json
 import random
 import requests
 
-# Ссылка на словарный запас из файла word.json
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+
+# Загрузка словарного запаса из файла word.json
 url = 'https://raw.githubusercontent.com/Nyxaris/Ellefire_AI/main/words.json'
 
 # Загрузка данных "words.json"
@@ -22,21 +28,69 @@ def save_data(data):
     with open("words.json", "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
-# Ввод вопроса и вывод ответа
+# Обработка запроса пользователя
+def process_input(user_input):
+    # Загрузка стоп-слов
+    stop_words = set(stopwords.words('english'))
+
+    # Токенизация запроса пользователя
+    tokens = word_tokenize(user_input)
+
+    # Удаление стоп-слов из запроса пользователя
+    filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+
+    # Обработка запроса с помощью обработчиков
+    matching_responses = []
+    for item in data:
+        for keyword in item.get('keywords', []):
+            if keyword.lower() in filtered_tokens:
+                matching_responses.append(item['response'])
+
+    # Возвращаем случайный ответ, если не найдено подходящего обработчика
+    if not matching_responses:
+        return random.choice(data)['response']
+
+    # Возвращаем случайный подходящий ответ
+    return random.choice(matching_responses)
+
+# Поиск информации в Интернете
+def search(query):
+    try:
+        # Выполнение GET-запроса к поисковому движку
+        response = requests.get('https://www.google.com/search?q=' + query)
+
+        # Проверка успешности запроса
+        if response.status_code == 200:
+            # Извлечение результата поиска
+            result = response.text
+            return result
+        else:
+            print("Ошибка:", response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Не удалось выполнить запрос:", e)
+        return None
+
+# Ввод запроса и вывод ответа
 while True:
     user_input = input("Вы: ")
     if user_input.strip() == "":
         continue
     response = None
-    # Поиск подходящего ответа в базе данных
-    matching_items = [item for item in data if item["message"].lower() in user_input.lower()]
-    if matching_items:
-        # Если заданный вопрос есть в базе данных то даём на него ответ
-        response = random.choice(matching_items)["response"]
+
+    # Обработка запроса пользователя
+    response = process_input(user_input)
+
+    # Если не найдено подходящего ответа, выполняем поиск в Интернете
+    if not response:
+        result = search(user_input)
+        if result:
+            print(result)
+        else:
+            print("Извините, не удалось найти ответ на ваш запрос.")
     else:
-        # Если заданного вопроса нет в базе данных то бот даст рандомный ответ
-        response = random.choice(data)["response"]
-    # Выбираем один из ответов на поставленный вопрос
-    if isinstance(response, list):
-        response = random.choice(response)
-    print("Nyx:", response)
+        # Вывод ответа
+        if isinstance(response, list):
+            print("Nyx:", random.choice(response))
+        else:
+            print("Nyx:", response)
