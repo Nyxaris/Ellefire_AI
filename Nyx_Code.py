7e16a4209691eda646a3a40bd5e2453ea7179af0
Nyxaris <85733843+Nyxaris@users.noreply.github.com>
2023-03-10 18:05:57 +0300

    Update Nyx_Code.py

--- Nyx_Code.py
+++ Nyx_Code.py
@@ -1,20 +1,14 @@
-# Автор: Nyxaris
-# Дата обновления: 10.03.23 
+# Автор : Nyxaris
+# Дата обновления : 10.03.2023
 
 import json
 import random
 import requests
 
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.download('punkt')
-
-# Загрузка словарного запаса из файла word.json
+# Ссылка на файл word.json в репозитории Github
 url = 'https://raw.githubusercontent.com/Nyxaris/Ellefire_AI/main/words.json'
 
-# Загрузка данных "words.json"
+# Загрузка данных из файла "words.json"
 try:
     response = requests.get(url)
     response.raise_for_status()
@@ -23,74 +17,26 @@ except requests.exceptions.RequestException as e:
     print("Не удалось загрузить данные:", e)
     data = []
 
-# Сохранение данных в файл "words.json"
+# Функция для сохранения данных в файл "words.json"
 def save_data(data):
     with open("words.json", "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
-# Обработка запроса пользователя
-def process_input(user_input):
-    # Загрузка стоп-слов
-    stop_words = set(stopwords.words('english'))
-
-    # Токенизация запроса пользователя
-    tokens = word_tokenize(user_input)
-
-    # Удаление стоп-слов из запроса пользователя
-    filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
-
-    # Обработка запроса с помощью обработчиков
-    matching_responses = []
-    for item in data:
-        for keyword in item.get('keywords', []):
-            if keyword.lower() in filtered_tokens:
-                matching_responses.append(item['response'])
-
-    # Возвращаем случайный ответ, если не найдено подходящего обработчика
-    if not matching_responses:
-        return random.choice(data)['response']
-
-    # Возвращаем случайный подходящий ответ
-    return random.choice(matching_responses)
-
-# Поиск информации в Интернете
-def search(query):
-    try:
-        # Выполнение GET-запроса к поисковому движку
-        response = requests.get('https://www.google.com/search?q=' + query)
-
-        # Проверка успешности запроса
-        if response.status_code == 200:
-            # Извлечение результата поиска
-            result = response.text
-            return result
-        else:
-            print("Ошибка:", response.status_code)
-            return None
-    except requests.exceptions.RequestException as e:
-        print("Не удалось выполнить запрос:", e)
-        return None
-
-# Ввод запроса и вывод ответа
+# Обработка ввода пользователя и вывод ответа от компьютера
 while True:
     user_input = input("Вы: ")
     if user_input.strip() == "":
         continue
     response = None
-
-    # Обработка запроса пользователя
-    response = process_input(user_input)
-
-    # Если не найдено подходящего ответа, выполняем поиск в Интернете
-    if not response:
-        result = search(user_input)
-        if result:
-            print(result)
-        else:
-            print("Извините, не удалось найти ответ на ваш запрос.")
+    # Поиск подходящего ответа в базе данных
+    matching_items = [item for item in data if item["message"].lower() in user_input.lower()]
+    if matching_items:
+        # Если есть подходящие ответы, то выбираем случайный из них
+        response = random.choice(matching_items)["response"]
     else:
-        # Вывод ответа
-        if isinstance(response, list):
-            print("Nyx:", random.choice(response))
-        else:
-            print("Nyx:", response)
+        # Если подходящего ответа нет, то даем рандомный ответ из базы данных
+        response = random.choice(data)["response"]
+    # Если ответ - список, выбираем из него случайный элемент
+    if isinstance(response, list):
+        response = random.choice(response)
+    print("Nyx:", response)
