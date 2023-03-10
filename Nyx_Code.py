c590193945370bc91d67306f1c4edef151876f45
Nyxaris <85733843+Nyxaris@users.noreply.github.com>
2023-03-10 18:44:41 +0300

    Update Nyx_Code.py
    Добавлена проверка доступности к интернету

--- Nyx_Code.py
+++ Nyx_Code.py
@@ -4,16 +4,32 @@
 import json
 import random
 import requests
+import time
 
 # Ссылка на файл word.json в репозитории Github
 url = 'https://raw.githubusercontent.com/Nyxaris/Ellefire_AI/main/words.json'
+
+# Проверка доступности интернет-соединения
+while True:
+    try:
+        requests.get("http://www.google.com", timeout=1)
+        # print("Интернет-соединение установлено")
+        break
+    except requests.exceptions.RequestException:
+        print("Интернет-соединение недоступно, ожидаем 10 секунд...")
+        time.sleep(10)
 
 # Загрузка данных из файла "words.json"
 try:
     response = requests.get(url)
     response.raise_for_status()
     data = json.loads(response.text)
-except requests.exceptions.RequestException as e:
+    if not isinstance(data, list):
+        raise ValueError("Некорректный формат данных в файле words.json")
+    for item in data:
+        if "message" not in item or "response" not in item:
+            raise ValueError("Некорректный формат данных в файле words.json")
+except (requests.exceptions.RequestException, ValueError) as e:
     print("Не удалось загрузить данные:", e)
     data = []
 
